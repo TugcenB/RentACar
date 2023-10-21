0a42aeba8c23a11ff653a745672e73771b14cdab
TugcenB <ttbilgin1@gmail.com>
2023-10-21 12:44:36 +0300

    Application Layer Implementation

---
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,42 @@
+﻿using Application.Services.Repositories;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.Create
+{
+    public class CreateBrandCommand : IRequest<CreatedBrandResponse>
+    {
+        public string Name { get; set; }
+
+        public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+        {
+            private readonly IBrandRepository _brandRepository;
+
+            public CreateBrandCommandHandler(IBrandRepository brandRepository)
+            {
+                _brandRepository = brandRepository;
+            }
+
+            public async Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                Brand brand = new();
+                brand.Name = request.Name;
+                brand.Id = Guid.NewGuid();
+
+                var result = await _brandRepository.AddAsync(brand);
+
+                CreatedBrandResponse createdBrandResponse = new();
+                createdBrandResponse.Name = result.Name;
+                createdBrandResponse.Id = result.Id;
+
+                return createdBrandResponse;
+            }
+        }
+
+    }
+}
