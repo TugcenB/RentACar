f9694affe9327ef22836d9bb5b6535209c7e4dc3
TugcenB <ttbilgin1@gmail.com>
2023-10-26 11:20:58 +0300

    Update Command CQRS Implementation

--- Application/Features/Brands/Profiles/MappingProfiles.cs
+++ Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Features.Brands.Commands.Create;
+using Application.Features.Brands.Commands.Update;
 using Application.Features.Brands.Queries.GetById;
 using Application.Features.Brands.Queries.GetList;
 using AutoMapper;
@@ -16,8 +17,13 @@ namespace Application.Features.Brands.Profiles
     public class MappingProfiles:Profile
     {
         public MappingProfiles()
-        {            CreateMap<Brand, CreateBrandCommand>().ReverseMap();
+        {            
+            CreateMap<Brand, CreateBrandCommand>().ReverseMap();
             CreateMap<Brand, CreatedBrandResponse>().ReverseMap();
+
+            CreateMap<Brand, UpdateBrandCommand>().ReverseMap();
+            CreateMap<Brand, UpdatedBrandResponse>().ReverseMap();
+
             CreateMap<Brand, GetListBrandListItemDto>().ReverseMap();
             CreateMap<Brand, GetByIdBrandResponse>().ReverseMap();
             CreateMap<Paginate<Brand>, GetListResponse<GetListBrandListItemDto>>().ReverseMap();
