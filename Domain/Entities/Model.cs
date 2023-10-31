a8a760dfe13258536b0836aa90a2578516da9052
TugcenB <ttbilgin1@gmail.com>
2023-10-31 12:21:34 +0300

    İlişkisel Domain Modelleme

--- Domain/Entities/Model.cs
+++ Domain/Entities/Model.cs
@@ -9,9 +9,34 @@ namespace Domain.Entities
 {
     public class Model: Entity<Guid>
     {
-        public int BrandId { get; set; }
+        public Guid BrandId { get; set; }
+        public Guid FuelId { get; set; }
+        public Guid TransmissionId { get; set; }
+        public string Name { get; set; }
         public decimal DailyPrice { get; set; }
         public string ImageUrl { get; set; }
+
+        public virtual Brand? Brand { get; set; }
+        public virtual Fuel? Fuel { get; set; }
+        public virtual Transmission? Transmission { get; set; }
+        public virtual ICollection<Car> Cars { get; set; }
+
+        public Model()
+        {
+            Cars = new HashSet<Car>();
+        }
+        public Model(Guid id, Guid brandId, Guid fuelId, Guid transmissionId, string name, decimal dailyPrice,string imageUrl):this()
+        {
+            Id = id;
+            BrandId = brandId;
+            FuelId = fuelId;
+            Name = name;
+            TransmissionId = transmissionId;
+            DailyPrice = dailyPrice;
+            ImageUrl = imageUrl;
+
+
+        }
     }
     
 }
