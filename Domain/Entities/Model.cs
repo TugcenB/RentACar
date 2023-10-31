1ae1a9e3963ce3cf05c7819a7aedf5cbf858ea43
TugcenB <ttbilgin1@gmail.com>
2023-10-31 11:53:20 +0300

    RentACar/Domain

---
+++ Domain/Entities/Model.cs
@@ -0,0 +1,17 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Model: Entity<Guid>
+    {
+        public int BrandId { get; set; }
+        public decimal DailyPrice { get; set; }
+        public string ImageUrl { get; set; }
+    }
+    
+}
