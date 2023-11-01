5b77715a95ab31f130746cb050c99e90034d250e
TugcenB <ttbilgin1@gmail.com>
2023-11-01 13:57:18 +0300

    Entity Configuration and Repository Implementation

--- Persistence/EntityConfigurations/BrandConfiguration.cs
+++ Persistence/EntityConfigurations/BrandConfiguration.cs
@@ -7,9 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Persistence.EntityConfigurations
-{
-    public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+namespace Persistence.EntityConfigurations;
+
+public class BrandConfiguration : IEntityTypeConfiguration<Brand>
     {
         public void Configure(EntityTypeBuilder<Brand> builder)
         {
@@ -21,7 +21,10 @@ namespace Persistence.EntityConfigurations
             builder.Property(b => b.UpdatedDate).HasColumnName("UpdatedDate");
             builder.Property(b => b.DeletedDate).HasColumnName("DeletedDate");
 
+            builder.HasIndex(indexExpression:b=>b.Name,name:"UK_Brands_Name").IsUnique();
+
+            builder.HasMany(b => b.Models);
+
             builder.HasQueryFilter(b=>!b.DeletedDate.HasValue);
         }
     }
-}
